17b9fae0dadf5a852b68bf8b8c8d733cd367504a
gadirlih <habil@wetravel.com>
2020-08-13 20:16:06 +0400

    change welcome message and test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I have changed
+          Someone changed me
         </a>
       </header>
     </div>
